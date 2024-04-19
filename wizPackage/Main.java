2bfd62d187c3f6a96248180cc9bc4ba25851ba37
srik6724 <112733278+srik6724@users.noreply.github.com>
2024-04-19 00:43:51 -0700

    test-commit

--- wizPackage/Main.java
+++ wizPackage/Main.java
@@ -3,7 +3,7 @@ package wizPackage;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[]args) throws InterruptedException, IOException
+	public static void main(String[]aYESrgs) throws InterruptedException, IOException
 	{ 
 		Match match = new Match(); 
 		match.enroll2Teams(); 
